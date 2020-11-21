4a484d01f9e509ff8b7b99e733ca4921bc420bf5
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2020-11-21 16:35:31 +0530

    Added name routes and arguments.

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:nav_meals/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
@@ -8,11 +7,10 @@ class CategoryItem extends StatelessWidget {
   CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext ctx) {
-    Navigator.of(ctx).push(MaterialPageRoute(
-      builder: (_) {
-        return CategoryMealsScreen(id, title);
-      },
-    ));
+    Navigator.of(ctx).pushNamed('/category-meals', arguments: {
+      'id': id,
+      'title': title,
+    });
   }
 
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nav_meals/categories_screen.dart';
+import 'package:nav_meals/category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,6 +23,9 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold),
               )),
       home: CategoriesScreen(),
+      routes: {
+        '/category-meals': (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
